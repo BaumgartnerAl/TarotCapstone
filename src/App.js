304babd946bb95c9de4c5c0e7e79e01f6f8659dc
abaumg14 <abaumg14@kent.edu>
2022-11-16 00:36:23 -0500

    button almost working

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import './App.css';
 import React, {useState, useEffect} from "react";
 import { tarotData } from "./tarotData";
 
-let tarotNum = 0;
 
 
 function App() {
 
   const [tarotNum, setTarotNum] = useState(1);
+  const [tarotID, setTarotID] = useState(1);
 
   function randomizeTarot(){
     setTarotNum(Math.floor((Math.random() * 11)))
+    setTarotID((Math.floor((Math.random() * 11)))+'.jpg')
     console.log(tarotNum)
     console.log(tarotData[tarotNum])
+    console.log(tarotID)
   }
 
   return (
@@ -22,6 +24,7 @@ function App() {
       <button onClick={randomizeTarot} class="readingbutton"> Generate </button>
       <p>{tarotNum}</p>
       <p>{tarotData[tarotNum].name}</p>
+      <img src={`/images/${tarotID}`} alt="tarot card"></img>
     </div>
   );
 }
