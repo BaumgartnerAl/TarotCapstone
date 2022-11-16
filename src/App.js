03d98d1e852bf31f3310c4b3a83fa8cd9a5f9acf
Alex <abaumg14@kent.edu>
2022-11-15 22:06:54 -0500

    Initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
+import { tarotData } from "./tarotData";
+
+let tarotNum = 0;
+
 
 function App() {
+
+  const [tarotNum, setTarotNum] = useState(1);
+
+  function randomizeTarot(){
+    setTarotNum(Math.floor((Math.random() * 11)))
+    console.log(tarotNum)
+    console.log(tarotData[tarotNum])
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <button onClick={randomizeTarot} class="readingbutton"> Generate </button>
+      <p>{tarotNum}</p>
+      <p>{tarotData[tarotNum].name}</p>
     </div>
   );
 }
---
+++ src/tarotData.js
@@ -0,0 +1,48 @@
+export const tarotData = [
+    {
+        id: 0,
+        name: "The Fool",
+        meaning: "sample text about meaning of card"
+    },
+    {
+        id: 1,
+        name: "The Magician",
+        meaning: "sample text"
+    },
+    {
+        id: 2,
+        name: "The High Priestess",
+    },
+    {
+        id: 3,
+        name: "The Empress"
+    },
+    {
+        id: 4,
+        name: "The Emperor"
+    },
+    {
+        id: 5,
+        name: "The Heirophant"
+    },
+    {
+        id: 6,
+        name: "The Lovers"
+    },
+    {
+        id: 7,
+        name: "The Chariot"
+    },
+    {
+        id: 8,
+        name: "Strength"
+    },
+    {
+        id: 9,
+        name: "The Hermit"
+    },
+    {
+        id: 10,
+        name: "Wheel of Time"
+    }
+]
