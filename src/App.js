035c35c84a3c391ef2d436c155c728cc275c8bb0
Alex <abaumg14@kent.edu>
2022-12-05 18:47:57 -0500

    app.js updated

--- src/App.js
+++ src/App.js
@@ -7,14 +7,12 @@ import { tarotData } from "./tarotData";
 function App() {
 
   const [tarotNum, setTarotNum] = useState(1);
-  const [tarotID, setTarotID] = useState(1);
 
   function randomizeTarot(){
     setTarotNum(Math.floor((Math.random() * 78)))
     // setTarotID((Math.floor((Math.random() * 11)))+'.jpg')
     console.log(tarotNum)
     console.log(tarotData[tarotNum])
-    console.log(tarotID)
   }
 
   return (
